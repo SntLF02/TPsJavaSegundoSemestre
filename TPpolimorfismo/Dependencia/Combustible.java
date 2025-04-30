dba29e4ea4a392891f0135a4e42478b7ece5b00d
Micaela Carolina <mikapaco14@gmail.com>
2025-04-30 10:12:10 -0300

    Add files via upload

---
+++ TPpolimorfismo/Dependencia/Combustible.java
@@ -0,0 +1,16 @@
+package Dependencia;
+
+public class Combustible {
+    private String tipo;
+
+    public Combustible(String tipo){
+        this.tipo = tipo;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
